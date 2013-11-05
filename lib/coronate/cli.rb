e4a15fae0068db0f66a58f9dd2c9b7593ce3a803
Eiffel Qiu <eiffelqiu@gmail.com>
2013-11-05 23:06:32 +0800

    add method options

--- lib/coronate/cli.rb
+++ lib/coronate/cli.rb
@@ -8,14 +8,21 @@ class CLI < Thor
       File.dirname(__FILE__) 
   end  
  
-  desc "scene", "generate an scene"
+  desc "scene [NAME]", "generate an scene"
   def scene(name='scene1')
     template 'templates/scene.tt', "#{name}.lua"
   end
 
-  desc "project", "generate a corona project"
+  desc "project [NAME]", "generate a corona project"
+
+  method_option :width, :type => :numeric, :default => 320, :required => false, :aliases => "-w", :desc => "width"
+  method_option :height, :type => :numeric, :default => 480, :required => false, :aliases => "-h", :desc => "height"
+  method_option :landscape, :type => :boolean, :default => true, :required => false, :aliases => "-l", :desc => "landscape or not"
   def project(name='project1')
     empty_directory  "#{name}/assets"
+    @width =  options[:width]
+    @height =  options[:height]
+    @orient =  options[:orient] ? %{ "landscapeLeft", "landscapeRight" } : %{ "portrait", "portraitUpsideDown" }
     template 'templates/main.tt', "#{name}/main.lua"
     template 'templates/utils.tt', "#{name}/utils.lua"
     template 'templates/titleScene.tt', "#{name}/titleScene.lua"
